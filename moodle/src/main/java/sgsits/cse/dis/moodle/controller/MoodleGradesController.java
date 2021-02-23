b24a3087a60d87768ba6828991c46cc932044a6e
pg30 <pranaygarg001@gmail.com>
2021-02-24 04:11:12 +0530

    New files related moodle grade service added

---
+++ moodle/src/main/java/sgsits/cse/dis/moodle/controller/MoodleGradesController.java
@@ -0,0 +1,12 @@
+package sgsits.cse.dis.moodle.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping(path ="/grades")
+public class MoodleGradesController {
+
+}
